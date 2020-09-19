f1f81cd61d8c79adf6ecace904002b1a97b04944
Andrey <AimDragMan2@yandex.ru>
2020-09-19 16:46:56 +0300

    modulesES6

---
+++ Theory/37_modulesES6/src/js/script.js
@@ -0,0 +1,15 @@
+// import { one as first} from './main';
+
+// console.log(first);
+// console.log(`${one} and ${two}`);
+//==================================================
+// import * as data from './main';
+
+// console.log(`${data.one} and ${data.two}`);
+// data.sayHi();
+//==================================================
+import { one, two } from './main';
+import sayHi from './main';
+
+console.log(`${one} and ${two}`);
+sayHi();
